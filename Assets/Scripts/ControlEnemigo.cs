7f83173298625e8efe6868b7cbe414ef7740eb7b
Asel <test@test.com>
2024-04-15 15:41:40 +0200

    hit a enemigo

--- Assets/Scripts/ControlEnemigo.cs
+++ Assets/Scripts/ControlEnemigo.cs
@@ -20,14 +20,18 @@ public class ControlEnemigo : MonoBehaviour
 
     public int vida = 3;
 
+    private bool beingHit = false;
+    private float navSpeed = 0f;
+    public bool attacking = false;
+
     // Start is called before the first frame update
     void Start()
     {
         navAgent = GetComponent<NavMeshAgent>();
         miAnimator = GetComponent<Animator>();
         miTransform = this.transform;
-        navAgent.SetDestination(puntos[index].transform.position); 
-
+        navAgent.SetDestination(puntos[index].transform.position);
+        navSpeed = navAgent.speed;
     }
 
     private void proximoPunto()
@@ -44,21 +48,29 @@ public class ControlEnemigo : MonoBehaviour
         parado = true;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+
+    private void Update()
     {
-        if(Vector3.Distance(miTransform.position, jugador.transform.position)<= maxDistJugador)
+        if (vida <= 0)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(miTransform.position, jugador.transform.position) <= maxDistJugador)
         {
             navAgent.SetDestination(jugador.transform.position);
             navAgent.speed = 6f;
         }
-        else if (Vector3.Distance(miTransform.position, navAgent.destination)<= maxDist)
+        else if (Vector3.Distance(miTransform.position, navAgent.destination) <= maxDist)
         {
             proximoPunto();
         }
+    }
 
+    void FixedUpdate()
+    {
         actualizarAnimator();
-        if (parado)
+        if (parado && !attacking)
         {
             crono += Time.deltaTime;
             if(crono >= tiempoActual)
@@ -90,16 +102,33 @@ public class ControlEnemigo : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.transform.tag.Equals("Sword"))
+        if(other.transform.tag.Equals("Player"))
+        {
+            attacking = true;
+            parado = true;
+            miAnimator.Play("Attack");
+        }
+    }
+
+    public void GetHit()
+    {
+        if (!beingHit && vida > 0)
         {
             miAnimator.Play("Head Hit");
             vida--;
+            beingHit = true;
         }
+    }
 
-        if(other.transform.tag.Equals("Player"))
-        {
-            miAnimator.Play("Attack");
-        }
+    public void StopHit()
+    {
+        beingHit = false;
+        attacking = false;
+        parado = false;
+    }
 
+    public void StopAttack()
+    {
+        attacking = false;
     }
 }
